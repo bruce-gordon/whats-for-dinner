143458ee1852af9a1c9b6c44f9a4cdc269a261df
Bruce Gordon <brc.gordon@gmail.com>
2020-09-03 10:39:05 -0600

    Update radio button and random functions

---
+++ data.js
@@ -0,0 +1,41 @@
+var name = 'Bruce'
+
+var sides = [
+  "Mac and cheese",
+  "Mashed potatoes",
+  "French fries",
+  "Spring rolls",
+  "Hot wings",
+  "Garlic bread",
+  "Chef sald",
+  "Hush puppies",
+  "Fried rice",
+  "Coleslaw"
+];
+
+var mainDishes = [
+  "Smoked ribs",
+  "Spaghetti",
+  "Grilled salmon",
+  "Green chile cheeseburgers",
+  "Grilled cheese",
+  "Enchiladas",
+  "Carnitas",
+  "Sushi",
+  "Drunken noodles",
+  "Kung pao chicken",
+  "Chicken tikka masala"
+];
+
+var desserts = [
+  "Ice cream",
+  "Lemon bars",
+  "Apple pie",
+  "Mochi",
+  "Flan",
+  "Canoli",
+  "Carrot cake",
+  "Cheesecake",
+  "Baklava",
+  "Macarons"
+];
